3c98680b459d43904d81f6607554e36548771df6
Peter McMaster <39925732+mcmasterml@users.noreply.github.com>
2024-01-22 00:19:02 -0800

    Remove comments from app.py

--- app.py
+++ app.py
@@ -47,39 +47,3 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
-
-
-'''
-from flask import Flask, render_template, request, redirect, url_for
-from datetime import datetime
-from utils import Database
-import mysql.connector
-import random
-import string
-
-
-def create_app():
-    app = Flask(__name__)
-    with app.app_context():
-        db_instance = Database('app_interactions')
-        db_instance.spawnAll()
-    return app
-
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def initialize():
-    return render_template('login.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def startSession():
-    Database.startSession()
-
-    return render_template('home.html')
-
-'''
